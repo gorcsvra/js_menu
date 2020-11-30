26d5a0a15b5d1bc324b479c99d378e51e650ef79
nagygabi <nagygabi1962@gmail.com>
2020-11-30 11:30:29 +0100

    gyakorlás

--- script.js
+++ script.js
@@ -13,6 +13,26 @@ function pageLoaded() {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tristique justo et fringilla posuere. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla odio eros, tempus ac nibh ut, tincidunt ultricies lorem. Etiam sapien magna, viverra vel porttitor sed, mollis id justo. Vivamus lacus nisl, molestie et egestas ac, mattis vel est. Maecenas venenatis nunc eros, sit amet ullamcorper nibh consectetur eu. Etiam nec pulvinar elit, vitae ultrices eros. Sed nec ef
         </p>
    `);
+
+   //Ilyen módon sosem teszünk rá css-t vagy style-t a html-elemünkre.
+   //document.getElementById("root").style.color = "blue";
+
+   //Ilyen módon kerüljük a class-hozzáadást.
+   //document.getElementById("root").className = "rootClass";
+   //document.getElementById("root").className += " welcome";
+
+   document.getElementById("root").classList.add("rootClass");
+   document.getElementById("root").classList.add("welcome");
+   document.getElementById("root").classList.add("haha");
+   document.getElementById("root").classList.remove("haha");
+   
+   //Ezzel kitöröljük, felülírjuk a html-elem teljes tartalmát.
+   //document.getElementById("root").innerHTML = "";
+   document.getElementById("root").classList.add("newClass");
+   
+   //Ezzel kitöröljük, felülírjuk a html-elem teljes tartalmát és saját magát.
+   //document.getElementById("root").outerHTML = "";
+
 }
 //Ez nem fog betöltődni.
 //document.getElementById("root").innerHTML = "<h1>world</h1>";
